54e1888facbf0ebab349360f129f3b7d484f6810
henrywicaksono12 <c14180014@john.petra.ac.id>
2020-04-22 09:12:54 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  constructor(private router : Router) {}
+
+  home2(){
+    this.router.navigate(['/home2']);
+  }
+
+  homepass() {
+    this.router.navigate(['/home', 3]);
+  }
+}
